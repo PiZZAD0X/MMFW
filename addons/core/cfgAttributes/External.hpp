4dde83937390fcd4c68779c558b21b09843fd172
PiZZAD0X <bravoecholimalima@gmail.com>
2019-08-17 00:25:30 -0700

    checkboxreversed attribute

--- addons/core/cfgAttributes/External.hpp
+++ addons/core/cfgAttributes/External.hpp
@@ -163,15 +163,3 @@ class SubTitleIndent: SubTitle {
         };
     };
 };
-
-class GVAR(CheckboxStateReversed): CheckboxState {
-    attributeLoad = QUOTE([ARR_3(_this,_value,_config)] call FUNC(CheckboxStateReversed_AttrLoad));
-    attributeSave = QUOTE([ARR_2(_this,_config)] call FUNC(CheckboxStateReversed_AttrSave));
-    class Controls: Controls {
-        class Title: Title {};
-        class Value: Value {
-            idc = 100;
-            onCheckedChanged = QUOTE(_this call FUNC(CheckboxStateReversed_onCheckedChanged));
-        };
-    };
-};
